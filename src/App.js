f74f7d556426b2914c607eff837dc92f36099b4a
Deepak-Kokane-bluepineapple <108118897+Deepak-Kokane-bluepineapple@users.noreply.github.com>
2022-10-17 00:40:26 +0530

    updated app.js using git

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1>Deployed app on Heroku</h1>
+       <div className='App'>
+        <h2>Making first change -  push to github - change on heroku  </h2>
+       </div>
       </header>
     </div>
   );
